5df4d1683cef582215fd8906eb2d060ab51c21a8
EHOTO <ktaewoong46@gmail.com>
2021-01-06 16:46:50 +0900

    Testing library named "HTML to Image"
    - Dom to image에서 개선된 라이브러리.
    - BannerMaker에 사용될 수 있는 라이브러리 테스트.

--- javascript/Open-Source/dom-to-image/src/App.js
+++ javascript/Open-Source/dom-to-image/src/App.js
@@ -1,24 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import Dom from './Dom';
+
+import * as htmlToImage from 'html-to-image';
+// import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+// import { download } from 'downloadjs';
 
 function App() {
+
+  function getBannerImage({ nodeId, width, height }) {
+    const sourceNode = document.getElementById(nodeId);
+
+    // htmlToImage.toPng(sourceNode)
+    //   .then(function (dataUrl) {
+    //     console.log(dataUrl);
+    //     var img = new Image();
+    //     img.src = dataUrl;
+    //     document.body.appendChild(img);
+    //     return dataUrl;
+    //   })
+    //   .catch(function (error) {
+    //     console.error('oops, something went wrong!', error);
+    //   });
+    htmlToImage.toJpeg(sourceNode, { quality: 1.0, pixelRatio: 1, width, height })
+      .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'banner.jpeg';
+        link.href = dataUrl;
+        link.click();
+      });
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="source" className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Test <code>HTML-to-Image</code>
+            <br/>
+            and get a PNG image base64-encoded data URL and display it right away!
+          </p>
+        </header>
+      </div>
+      <button
+        type="button"
+        onClick={() => getBannerImage({ nodeId: 'source', width: 250, height: 250})}
+      >
+        Get React Image
+      </button>
+      <Dom />
+      <button
+        type="button"
+        onClick={() => getBannerImage({ nodeId: 'preview', width: 250, height: 250})}
+      >
+        Get Preview Image
+      </button>
+    </>
   );
 }
 
---
+++ javascript/Open-Source/dom-to-image/src/Dom.js
@@ -0,0 +1,9 @@
+import './Dom.css';
+
+export default function Dom() {
+  return (
+    <div id="preview">
+      <h2>Banner Topic</h2>
+    </div>
+  );
+}
